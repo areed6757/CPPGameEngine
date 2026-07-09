efe17e06ddf6c717d32d51ec54e10eb0b90c0bc2
areed6757 <areed6757@gmail.com>
2026-07-09 16:30:02 -0400

    Locked Vector3 Z-axis for 2D implementation

--- Engine/Include/Physics/Vector3.h
+++ Engine/Include/Physics/Vector3.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <Core/Common.h>
 
+// Vector3 with no Z-axis controls, for 2D implementation in 3D space.
 namespace Engine {
 	class Vector3 {
 	public:
 		Vector3();
-		Vector3(d64 x, d64 y, d64 z);
+		Vector3(d64 x, d64 y);
 		~Vector3();
 		
 	private:
--- Engine/Source/Physics/Vector3.cpp
+++ Engine/Source/Physics/Vector3.cpp
@@ -5,7 +5,8 @@ Engine::Vector3::Vector3() : m_x{0.0}, m_y{0.0}, m_z{0.0}
 
 }
 
-Engine::Vector3::Vector3(d64 x, d64 y, d64 z) : m_x{x}, m_y{y}, m_z{z}
+// This Vector3 is Z-locked for 2d implementation in a 3d space.
+Engine::Vector3::Vector3(d64 x, d64 y) : m_x{x}, m_y{y}, m_z{ 0.0 }
 {
 }
 
