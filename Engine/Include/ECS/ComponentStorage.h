618ca54056d800b00daca0101a296a33c40aa2b6
areed6757 <areed6757@gmail.com>
2026-07-12 09:36:07 -0400

    Partially completed ComponentStorage.h

--- Engine/Include/ECS/ComponentStorage.h
+++ Engine/Include/ECS/ComponentStorage.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Core/Common.h>
+#include <format>
 
 namespace Engine {
 	constexpr i32 INVALID_INDEX{ -1 };
@@ -7,13 +8,23 @@ namespace Engine {
 	template <typename T>
 	class ComponentStorage : public Base {
 	public:
-		explicit ComponentStorage(const ComponentStorageDesc& desc) {
-			m_sparse.assign{desc.maxEntities + 1, INVALID_INDEX};
+		explicit ComponentStorage(const ComponentStorageDesc& desc) : Base(desc.base) {
+			m_maxEntities = desc.maxEntities;
+			m_sparse.assign(m_maxEntities + 1, INVALID_INDEX);
 		}
-		~ComponentStorage();
+		~ComponentStorage() {};
 
-		void add(i32 index, const T& value) {
-
+		void add(i32 index, const T& component) {
+			if (!(index > 0 && index <= m_maxEntities)) {
+				EngineLogError(std::format("Invalid attempt to add component to index {}", index).c_str()); 
+				return;
+			}
+			if (m_sparse.at(index) != INVALID_INDEX) {
+				EngineLogWarning(std::format("Entity {} already has this component; overwriting", index).c_str());
+			}
+			m_dense.push_back(component);
+			m_backRef.push_back(index);
+			m_sparse.at(index) = static_cast<i32> (m_dense.size() - 1);
 		}
 
 		void remove(i32 index) {
@@ -29,6 +40,7 @@ namespace Engine {
 		}
 	
 	private:
+		i32 m_maxEntities{};
 		std::vector<i32> m_sparse{};
 		std::vector<T> m_dense{};
 		std::vector<i32> m_backRef{};
