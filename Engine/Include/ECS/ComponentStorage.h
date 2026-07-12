14b5b26faa388e4c09a5666ea27aa61588437d50
areed6757 <areed6757@gmail.com>
2026-07-12 14:03:52 -0400

    Completed basic creation of ComponentStorage

--- Engine/Include/ECS/ComponentStorage.h
+++ Engine/Include/ECS/ComponentStorage.h
@@ -20,7 +20,8 @@ namespace Engine {
 				return;
 			}
 			if (m_sparse.at(index) != INVALID_INDEX) {
-				EngineLogWarning(std::format("Entity {} already has this component; overwriting.", index).c_str());
+				EngineLogWarning(std::format("Entity {} already has this component.", index).c_str());
+				return;
 			}
 			m_dense.push_back(component);
 			m_backRef.push_back(index);
@@ -28,7 +29,18 @@ namespace Engine {
 		}
 
 		void remove(i32 index) {
+			assert(has(index) && "remove() called on entity with no component");
+			i32 denseIndex = m_sparse.at(index);
+			i32 lastDenseIndex = static_cast<i32>(m_dense.size() - 1);
+			i32 lastEntity = m_backRef.at(lastDenseIndex);
 
+			m_dense.at(denseIndex) = m_dense.at(lastDenseIndex);
+			m_backRef.at(denseIndex) = lastEntity;
+			m_sparse.at(lastEntity) = denseIndex;
+
+			m_dense.pop_back();
+			m_backRef.pop_back();
+			m_sparse.at(index) = INVALID_INDEX;
 		}
 
 		bool has(i32 index) const noexcept {
@@ -38,6 +50,10 @@ namespace Engine {
 		T& get(i32 index) {
 			assert(has(index) && "get() called on entity with no component");
 			return m_dense.at(m_sparse.at(index));
+		}
+
+		i32 size() {
+			return m_dense.size();
 		}
 	
 	private:
