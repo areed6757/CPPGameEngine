082bebaf4c6d5be03bb1d17023936299c7306916
areed6757 <areed6757@gmail.com>
2026-07-14 22:55:59 -0400

    Comments everywhere for Renderer, deleting once learned

--- Engine/Source/Graphics/Renderer.cpp
+++ Engine/Source/Graphics/Renderer.cpp
@@ -23,20 +23,33 @@ GLfloat vertices[] =
 
 Engine::Renderer::Renderer(const RendererDesc& desc) : Base(desc.base), m_window(desc.window)
 {
+    // Vertex shaders process point indices to determine where they should be placed on screen
+    // (Also, they prepare lighting and texture coords for next step)
+    // Create vertex shader and get its reference
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // Attach vertex shader source to vertex shader object
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    // Compile vertex shader -> machine code
     glCompileShader(vertexShader);
 
+    // Fragment shaders determine the exact color and appearance of every pixel on screen
+    // Create fragment shader and get its reference
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    // Attach fragment shader source to vertex shader object
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    // Compile fragment shader -> machine code
     glCompileShader(fragmentShader);
 
+    // Create shader program object, get ref
     m_shaderProgram = glCreateProgram();
+    // Attach vertex+fragment shaders to the program
     glAttachShader(m_shaderProgram, vertexShader);
     glAttachShader(m_shaderProgram, fragmentShader);
 
+    // Wrap-up/link all shaders into the program
     glLinkProgram(m_shaderProgram);
 
+    // Delete the now useless vertex and fragment shader objects (they live in the program now)
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
@@ -44,22 +57,22 @@ Engine::Renderer::Renderer(const RendererDesc& desc) : Base(desc.base), m_window
     // Vertex Buffer Object (VBO) - actually normally an array of references not just this one reference integer, passed to GPU to process a buffer
     // ALWAYS generate VAO first!
 
-    glGenVertexArrays(1, &m_VAO);
-    glGenBuffers(1, &m_VBO);
-    glBindVertexArray(m_VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+    glGenVertexArrays(1, &m_VAO);   //make VAO w/ 1 object only
+    glGenBuffers(1, &m_VBO);        //make VBO w/ 1 object only
+    glBindVertexArray(m_VAO);       // makes the VAO the current VAO by binding it
+    glBindBuffer(GL_ARRAY_BUFFER, m_VBO); // bind VBO, define its type
     // Store vertices in the buffer object
     // the last arg can be STATIC_, STREAM_, DYNAMIC_ and _DRAW, _READ, _COPY
     // _STATIC_ is drawn once and used many many times
     // _STREAM_ is drawn once and used a few times
     // _DYNAMIC_ is modified many times and used many times
     // _DRAW means the vertices will be modified and used to draw an image on the screen
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Introduce vertices to the VBO
     // Vertex attribute is a way of communicating with a Vertex shader from the outside
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); // Configs Vertex Attribute so OpenGL can read the VBO
+    glEnableVertexAttribArray(0); // Enables Vertex Attribute so OpenGL knows to use it
     // Unbind to prevent changing buffers VBO AND VAO with a function
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);   // Bind VBO and VAO to 0 to avoid accidentally modifying them
     glBindVertexArray(0);
 }
 
@@ -72,11 +85,15 @@ Engine::Renderer::~Renderer()
 
 void Engine::Renderer::draw()
 {
+    // Set a static color
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f );
     glClear( GL_COLOR_BUFFER_BIT );
 
+    // Tell OpenGL which program to use
     glUseProgram( m_shaderProgram );
+    // Bind VAO so it is used
     glBindVertexArray( m_VAO );
+    // Draw object w/ GL_TRIANGLES primitive
     glDrawArrays( GL_TRIANGLES, 0, 3 );
 
     glfwSwapBuffers( m_window.get() );
